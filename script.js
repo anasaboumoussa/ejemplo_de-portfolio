33d1fce38dfbfa6f82ffdb661075cfba7cc5280e
anasaboumoussa <aaboumoussa229@gmail.com>
2025-02-18 11:04:30 +0000

    he subido las paginas que he hecho

---
+++ script.js
@@ -0,0 +1,14 @@
+let frutas = ["manzana" , "lemon" , "pera","lemon" ]
+console.log(frutas[2])
+for(let i = 0 ; i <frutas.length; i++){
+
+    console.log(frutas[i])
+}
+
+
+
+
+for(let i = 1; i<=10; i++){
+
+    console.log(i*2);
+}
